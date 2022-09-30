f8666a4aa88f82f71f657c204b811374b7ce88e0
Avanes <avanesovkl@mail.ru>
2022-10-01 02:03:12 +1100

    Add Homework #1

---
+++ HW1/EXX02.py
@@ -0,0 +1,10 @@
+import os
+
+# Очистка терминала для удобства восприятия.
+os.system('cls')
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (x or y or z) == (not x and not y and not z):
+                print(x, y, z)
